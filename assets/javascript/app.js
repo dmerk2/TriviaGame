a26e40611883311db87a82c053d2398a013bfb31
Dan Merkin <danmerkin@Dans-MBP.home>
2016-05-30 12:34:47 -0400

    updated3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,29 @@
 
 var incorrect=false;
 var correct=true;
-var wins wins =+ 1;
-var losses;
-var total;
+var wins= 0;
+var losses= 0;
+var total= 0;
 var userGuess = '';
+var timer;
+//When document starts this function will happen
+$(document).ready(function(){	
 
 
+
+//restarts game
+function restart(){
+	var incorrect=false;
+	var correct=true;
+	var wins= 0;
+	var losses= 0;
+	var total= 0;
+	var userGuess = '';
+	var timer;
+
+	//adds text to wins and losses
+	$("#userGuess").text(total);
+	$("#wins").text(wins);
+	$("#losses").text(losses);
+	
 
