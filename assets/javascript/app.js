53d9fe3aa9cadcd58d948fba395ca82f87c33159
Dan Merkin <danmerkin@Dans-MBP.home>
2016-05-30 17:59:08 -0400

    Updated4

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,15 @@
 //alert("See if you can guess the right answer!")
 
-var incorrect=false;
-var correct=true;
-var wins= 0;
-var losses= 0;
-var total= 0;
+var incorrect = false;
+var correct = true;
+var unanswered = 0;
 var userGuess = '';
-var timer;
-//When document starts this function will happen
-$(document).ready(function(){	
+var booleanValue = '';
 
-
-
-//restarts game
-function restart(){
-	var incorrect=false;
-	var correct=true;
-	var wins= 0;
-	var losses= 0;
-	var total= 0;
-	var userGuess = '';
-	var timer;
-
-	//adds text to wins and losses
-	$("#userGuess").text(total);
-	$("#wins").text(wins);
-	$("#losses").text(losses);
-	
-
+$(document).ready(function() {
+  $('#start').click(function() {
+  	$('#start').addClass('hide');
+    $('.questions').removeClass('hide');
+    $('#musicPic').addClass('hide');
+  });
+});
