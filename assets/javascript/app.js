4ec1635a2a69f8dea9e4c9d3b60a15085c5936cc
Dan Merkin <danmerkin@Dans-MBP.home>
2016-05-29 20:44:10 -0400

    updated2

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+alert("Pick one answer per question!")
+console.log(alert)
+
+var wins;
+var losses;
+var total;
+var userGuess = true || false;
+
+//When the document loads this function will happen
+$(document).ready(function(){
+	
+	if(q1 === 'a'){
+		alert("correct");
+	}	else	{
+		alert("incorrect");
+	};
+
