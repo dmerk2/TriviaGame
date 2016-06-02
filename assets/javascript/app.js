07c0fad6c3f30bcec04589fd14cc476f9e7afc98
Dan Merkin <danmerkin@Dans-MBP.home>
2016-06-02 18:28:44 -0400

    correct timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 //HOW DO I ALERT ONLY 1 PAGE??            
-//alert("See if you can guess the right answer!")
+alert("You have 30 seconds after pushing start to finish!")
 
 var incorrect = false;
 var correct = true;
@@ -7,9 +7,8 @@ var pointsCorrect = [];
 var pointIncorrect = [];
 var userClick = '';
 var questionNumber;
-var timer;
-
-
+var timeRemaining = 30;
+var intervalId;
 //when the document loads this will happen
 $(document).ready(function() {
 	//when you click start..
@@ -20,17 +19,26 @@ $(document).ready(function() {
     //hides musicPic
     $('#musicPic').addClass('hide');
     // Start Timer
-    setTimeout(timer, 3 * 1000);
+      setTimeout(timer, 30 * 1000);
+      intervalId = setInterval(showTimeLeft, 1000);
     });
 });
 //After 30 seconds alert 'Game Over' and Goes to action_page.html
 function timer() {
   alert('Game Over!');
-  setTimeout(window.location.assign('action_page.html', 15 * 1000));
   //go to action_page.hml
-
+  $('#results').removeClass('hide');
+  $('.questions').addClass('hide');
+  //setTimeout(window.location.assign('action_page.html', 30 * 1000));
 };
 
+function showTimeLeft() {
+  timeRemaining--;
+  $('#timer').html(timeRemaining)
+    if (timeRemaining === 0){
+      clearInterval(intervalId)
+    };
+}
 
 // var questionNumber = $('input').attr('name');
 // 	if (questionNumber ==='q1')
